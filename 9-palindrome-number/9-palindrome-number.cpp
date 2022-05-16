b26cbbd2479c9585bedcec3eb81ca38d7467a42d
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-05-16 02:35:11 +0200

    Time: 12 ms (79.97%), Space: 5.9 MB (33.98%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return 0;
+        
+        string s = to_string(x);
+        int n = s.size();
+        for(int i=0; i<n/2; i++)
+        {
+            if(s[i] != s[n-i-1])
+            {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
