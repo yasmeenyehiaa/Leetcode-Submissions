a523c083eef4b32a4a7267ac1248ec8aa1938e55
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-05-23 02:17:09 +0200

    Time: 33 ms (77.22%), Space: 24.9 MB (92.80%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void rotateSegment(int l, int r, vector<int>& nums)
+    {
+        while(l < r)
+        {
+            int tmp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = tmp;
+            
+            l++, r--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        
+        
+        int n = nums.size();
+        k = k%n;
+        rotateSegment(0,n-1, nums);
+        rotateSegment(0,k-1 ,nums);
+        rotateSegment(k,n-1, nums);
+        
+        
+    }
+};
