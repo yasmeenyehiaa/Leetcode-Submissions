59a651750f7cd8f3bbd32e80c447afcf36d8926c
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-05-31 20:56:41 +0200

    Time: 8 ms (79.57%), Space: 7.4 MB (8.73%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.size() != t.size())
+        {
+            return false;
+        }
+        unordered_map<char, int> cnt_s, cnt_t;
+        for(int i=0; i<s.size(); i++)
+        {
+            cnt_s[s[i]]++;
+            cnt_t[t[i]]++;
+        }
+        
+        for(auto ch: t)
+        {
+            if(cnt_t[ch] != cnt_s[ch])
+            {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
