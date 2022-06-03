3b3ae00c9be3a981b13407cc0b09624c79875949
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-06-03 17:42:15 +0200

    Time: 15 ms (9.06%), Space: 15.5 MB (6.20%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         
-        string ans = "";
-        
+        string ans = "";        
         for(int i=0; i<strs[0].size(); i++)
         {
             char ch = strs[0][i];
