d7991db896fe342f293ec4c811a28cff3cea69d7
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-06-03 17:24:19 +0200

    Time: 308 ms (5.85%), Space: 19.3 MB (58.64%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int n = nums.size();
+        for(int i=0; i<n-1; i++)
+        {
+            if(nums[i] == 0)
+            {
+                int j = i;
+                while(j<n && nums[j] == 0)
+                {
+                    j++;
+                }
+                if(j == n)
+                {
+                    break;
+                }
+                nums[i] = nums[j];
+                nums[j] = 0;
+            }
+        }
+    }
+};
