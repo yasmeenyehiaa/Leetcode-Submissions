ca2230bb492775457d3f8ccc560c498eee99b471
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-06-03 15:28:16 +0200

    Time: 149 ms (15.88%), Space: 69.3 MB (89.10%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         
-        int tot = 0 , index = 0 , mn = 0;
+        int tot = 0, index = 0, mn = 0;
         for(int i=0; i<gas.size(); i++)
         {
             tot += gas[i] - cost[i];
@@ -11,13 +11,11 @@ public:
                 index = i+1;
                 mn = tot;
             }   
-        }
-        
+        }        
         if(tot >= 0)
         {
             return index;
-        }
-        
+        }        
         else return -1;
     }
 };
