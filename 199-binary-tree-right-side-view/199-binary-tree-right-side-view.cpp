4bcab22e3cd8b68c09b8cc9545e914d7e43fb19d
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-06-11 22:43:48 +0200

    Time: 0 ms (100.00%), Space: 12 MB (45.57%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -24,7 +24,8 @@ public:
             int sz = q.size();
             for(int i=0; i<sz; i++)
             {
-                TreeNode* top = q.front();    q.pop();
+                TreeNode* top = q.front();    
+                q.pop();
                 if(i == sz-1)
                 {
                     ans.push_back(top->val);
