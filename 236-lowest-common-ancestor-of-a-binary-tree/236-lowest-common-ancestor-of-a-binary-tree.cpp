fb04ee29ba0281940f4572569ac2f07cae670048
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-06-11 20:48:32 +0200

    Time: 34 ms (15.52%), Space: 14.3 MB (27.17%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -14,6 +14,7 @@ public:
         if(!root || root == p || root == q) return root;   
         
         TreeNode* left  = lowestCommonAncestor(root->left, p, q);
+        
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
                
         if(!left && !right) return NULL;
