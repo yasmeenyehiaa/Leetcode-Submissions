9908a768d4f487c146c708e0a2873f29351ac78d
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-06-19 01:11:19 +0200

    Time: 9 ms (48.80%), Space: 10.8 MB (20.71%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
-    {        
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        
         map<int,int> cnt1;
-        vector<int> ans;        
+        vector<int> ans;
         for(int i=0; i<nums1.size(); i++)
         {
             cnt1[nums1[i]]++;
-        }            
+        }
         for(int i=0; i<nums2.size(); i++)
         {
             if(cnt1[nums2[i]])
@@ -15,7 +15,9 @@ public:
                 ans.push_back(nums2[i]);
                 cnt1[nums2[i]]--;
             }
-        }        
-        return ans;        
+        }
+        
+        return ans;
+        
     }
 };
