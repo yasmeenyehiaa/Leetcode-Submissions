7544320b2c032373a3f841ae142858188427b511
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-06-20 04:38:49 +0200

    Time: 51 ms (64.26%), Space: 32.2 MB (98.25%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -17,8 +17,7 @@ public:
         if(root2 == NULL) return root1;        
         root1->val += root2->val;        
         root1->left = mergeTrees(root1->left, root2->left);
-        root1->right = mergeTrees(root1->right, root2->right);
-        
+        root1->right = mergeTrees(root1->right, root2->right);        
         return root1;
     }
 };
