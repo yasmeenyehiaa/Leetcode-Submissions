af6a39f2c5eafb39a4d8d84f38684998d3ea7066
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-01 16:56:26 +0200

    Time: 14 ms (66.49%), Space: 8.5 MB (98.01%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int m,n;
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        m = mat.size(), n = mat[0].size();
-        
+        m = mat.size(), n = mat[0].size();        
         for(int c=0; c<n; c++)
         {
             sort(mat, 0, c);
@@ -22,10 +21,8 @@ public:
         while(r<m && c<n)
         {
             arr[mat[r++][c++]]++;
-        }
-        
+        }        
         r=row, c =col;
-        
         for(int i=1; i<101; i++)
         {
             if(arr[i])
