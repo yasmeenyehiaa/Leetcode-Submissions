c864175743cea955ba2927c880804f6694faa2b2
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-01 16:34:37 +0200

    Time: 3 ms (96.49%), Space: 10.9 MB (10.95%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -8,7 +8,8 @@ public:
         for(int i=0; i<nums1.size(); i++)
         {
             s1.insert(nums1[i]);
-        }        
+        }
+        
         for(int i=0; i<nums2.size(); i++)
         {
             s2.insert(nums2[i]);
