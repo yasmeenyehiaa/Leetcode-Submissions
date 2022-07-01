e2d88a6ef6242cccb3149c3c2080ce3d6ea72c6e
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-01 20:26:27 +0200

    Time: 36 ms (59.32%), Space: 14.1 MB (15.42%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -15,10 +15,10 @@ ListNode* mergeKLists(vector<ListNode*>& lists) {
     priority_queue<int,vector<int>,greater<int>>pq;
     ListNode* start = NULL, *end = NULL;
     
-    for(int i=0;i<lists.size();i++)
+    for(int i=0; i<lists.size(); i++)
     {
         ListNode* head = lists[i];
-        while(head!=NULL)
+        while(head != NULL)
         {
             pq.push(head->val);
             head = head->next;
