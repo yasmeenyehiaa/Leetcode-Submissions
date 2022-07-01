cf3b27821b349a0c8357ce18e2ad609c15d4eef4
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-01 18:47:59 +0200

    Time: 10 ms (17.94%), Space: 7.7 MB (82.88%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        
+        map<int,int> frq;
+        for(int i=0; i<arr1.size(); i++)
+        {
+            frq[arr1[i]]++;
+        }
+        
+        int j = 0;
+        for(int i=0; i<arr2.size(); i++)
+        {
+            while(frq[arr2[i]])
+            {
+                arr1[j++] = arr2[i];
+                frq[arr2[i]]--;
+            }
+        }
+        
+        for(auto p: frq)
+        {
+            for(int i=0; i<p.second; i++) arr1[j++] = p.first;
+        }
+        
+        return arr1;
+    }
+};
