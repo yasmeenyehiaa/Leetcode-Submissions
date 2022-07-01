0eaa938a2138cd19baf2543715aef7781d6eecf9
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-01 17:11:41 +0200

    Time: 301 ms (69.94%), Space: 72.1 MB (35.45%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    float dist(int x, int y)
+    {
+        return sqrt(x*x + y*y);
+    }
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        
+        vector<vector<int>> ans;
+        
+        priority_queue<pair<float, pair<int,int>>> pq;
+        
+        for(int i=0; i<points.size(); i++)
+        {
+            pq.push({-dist(points[i][0],points[i][1]), {points[i][0],points[i][1]}});
+        }
+        
+        int x = 0;
+        while(x++ < k)
+        {
+            vector<int> v;
+            v.push_back(pq.top().second.first);
+            v.push_back(pq.top().second.second);
+            pq.pop();
+            ans.push_back(v);
+        }
+        
+        return ans;
+    }
+};
