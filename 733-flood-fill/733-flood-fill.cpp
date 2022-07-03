afcc066d838ab78e5c0b1a5c0bdd8d2833630317
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-03 22:59:13 +0200

    Time: 7 ms (94.72%), Space: 14 MB (80.00%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -3,7 +3,7 @@ public:
     int c, o, n, m, dx[4] = {0,1,-1,0} , dy[4] = {1,0,0,-1};
     bool valid(int x, int y)
     {
-        return x>=0 && x<n && y>=0 && y<m;
+        return (x>=0 && x<n && y>=0 && y<m);
     }
     void dfs(vector<vector<int>>& image, int x, int y)
     {
