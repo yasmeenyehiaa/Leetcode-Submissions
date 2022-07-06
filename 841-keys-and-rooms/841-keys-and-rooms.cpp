0a42e89835869e9659f0e44c14449bf6e5ca6586
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-06 23:14:49 +0200

    Time: 8 ms (90.20%), Space: 10.3 MB (78.89%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -13,10 +13,12 @@ public:
             vis[x] = 1;
             for(int i=0; i<rooms[x].size(); i++)
             {
-                if(!vis[rooms[x][i]])  q.push(rooms[x][i]);
+                if(!vis[rooms[x][i]])  
+                    q.push(rooms[x][i]);
             }
         }    
-        for(int i=0; i<=n; i++) if(vis[i]) ctr++;
+        for(int i=0; i<=n; i++) 
+            if(vis[i]) ctr++;
         return (ctr == n);
     }
 };
