73bc7728a52258422b6d2ffbf121b693005ae91b
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-09 21:00:58 +0200

    Time: 617 ms (7.63%), Space: 132.4 MB (21.83%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -6,7 +6,7 @@ public:
         multiset<int, greater<int>> s;
         for(int i=0; i<n; i++)
         {
-            int val = s.size() == 0? 0 : *s.begin();
+            int val = s.empty()? 0 : *s.begin();
             nums[i] += val;
             s.insert(nums[i]);
             if(s.size() > k) s.erase(s.find(nums[i-k]));
