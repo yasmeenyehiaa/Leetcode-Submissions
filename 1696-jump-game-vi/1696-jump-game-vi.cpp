114aaea4fcf6fe9a0ea6b3dfa3f634c75b0cda6f
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-09 21:10:17 +0200

    Time: 291 ms (57.01%), Space: 77.4 MB (93.99%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -3,8 +3,7 @@ public:
     int maxResult(vector<int>& nums, int k) {
         int n = nums.size();
         deque<int> d{0};
-        for(int i=1; i<n; i++)
-        {
+        for(int i=1; i<n; i++) {
             if(d.front()+k < i) 
                 d.pop_front();
             nums[i] += nums[d.front()];
