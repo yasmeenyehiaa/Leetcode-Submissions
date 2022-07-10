2d443f1f90bde8b06ebc1f70cfb7b0b33832f4cb
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-10 06:33:59 +0200

    Time: 24 ms (79.66%), Space: 17.8 MB (29.03%) - LeetHub

--- 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
@@ -3,9 +3,9 @@ public:
     void wiggleSort(vector<int>& nums) {
         int n = nums.size();
         vector<int> tmp(n);
-        sort(nums.begin(), nums.end());
         int i = n/2, j = n-1, k = 0;
         if(n%2 == 0) i--;
+        sort(nums.begin(), nums.end());
         while( (i >= 0 || j > i) && k < n)
         {
             if(k%2) tmp[k++] = nums[j--];
