3418580edd6b3247b9ba447f1f51e9c117757610
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-14 13:58:19 +0200

    Time: 10 ms (12.90%), Space: 7.9 MB (34.06%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.cpp
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -8,8 +8,7 @@ public:
             else if(i%3 == 0) ans.push_back("Fizz");
             else if(i%5 == 0) ans.push_back("Buzz");
             else ans.push_back(to_string(i));
-        }
-        
+        }        
         return ans;        
     }
 };
