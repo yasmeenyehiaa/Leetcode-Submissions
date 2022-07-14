9f18cc4ceb812ae64b59bd3faff90815ed75081e
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-14 13:44:44 +0200

    Time: 17 ms (90.89%), Space: 26.4 MB (47.35%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -17,8 +17,8 @@ public:
     {
         if(st > ed) return NULL;
         int el = preorder[x++], pos = mp[el];
-        TreeNode *root = new TreeNode(el);
         
+        TreeNode *root = new TreeNode(el);
         root->left  = solve(st, pos-1, preorder, inorder);
         root->right = solve(pos+1, ed, preorder, inorder);
         
