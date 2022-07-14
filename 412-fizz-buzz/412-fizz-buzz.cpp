4b2f00c6aa9f14f14b1e64d0d7a375339ab4b669
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-14 13:54:39 +0200

    Time: 5 ms (51.18%), Space: 7.9 MB (63.57%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.cpp
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -5,8 +5,8 @@ public:
         for(int i=1; i<=n; i++)
         {
             if(i%5 ==0 && i%3 == 0) ans.push_back("FizzBuzz");
-            else if(i%5 == 0) ans.push_back("Buzz");
             else if(i%3 == 0) ans.push_back("Fizz");
+            else if(i%5 == 0) ans.push_back("Buzz");
             else ans.push_back(to_string(i));
         }
         return ans;        
