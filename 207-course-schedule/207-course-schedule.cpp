397638e732aa92fd462d506a2334850e208f078c
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-19 08:49:00 +0200

    Time: 29 ms (67.95%), Space: 14.5 MB (30.41%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> adj[2005];
+    bool vis[2005];
+    bool isCyclic(int src, set<int> &stack)
+    {
+        vis[src] = 1;
+        stack.insert(src);
+        for(auto a:adj[src])
+        {
+            if (stack.find(a) != stack.end()) return true;
+            if(!vis[a] && isCyclic(a, stack)) return true;
+        }
+        stack.erase(*stack.find(src));
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+       
+        int n = prerequisites.size();
+        for(int i=0; i<prerequisites.size(); i++)
+        {
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        
+        for(int i=0; i<numCourses; i++)
+        {
+            if(!vis[i])
+            {
+                set<int> st;
+                if(isCyclic(i, st)) return false;
+            }
+        }
+        return true;
+        
+    }
+};
