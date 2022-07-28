e8f6f8e1690d06d472f45ba7d4923ceb2c0fbb59
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-28 04:05:18 +0200

    Time: 12 ms (65.55%), Space: 7.4 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -11,8 +11,7 @@ public:
         {
             cnt_s[s[i]]++;
             cnt_t[t[i]]++;
-        }
-        
+        }        
         for(auto ch: t)
         {
             if(cnt_t[ch] != cnt_s[ch])
@@ -20,7 +19,6 @@ public:
                 return false;
             }
         }
-        return true;
-        
+        return true;        
     }
 };
