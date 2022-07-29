01cfa5b6feaa7d23ab0b49ec0ab006d078d716c3
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-07-29 17:28:10 +0200

    Time: 6 ms (62.68%), Space: 8.3 MB (65.33%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector<string> ans;
+        for(auto w: words)
+        {
+            if(w.size() != pattern.size()) continue;
+            map<char,char> mp;
+            bool ok = 1;
+            int i = 0;
+            for(auto ch: w)
+            {
+                if(mp[pattern[i]])
+                {
+                    ok = ok && (ch == mp[pattern[i]]);
+                }
+                else mp[pattern[i]] = ch;
+                i++;
+            }
+            bool vis[28] = {0};
+            for(auto p: mp)
+            {
+                char ch = p.second;
+                if(vis[ch - 'a']) {ok = 0; break;}
+                vis[ch - 'a'] = 1;
+            }
+            if(ok) ans.push_back(w);
+        }
+        return ans;
+    }
+};
