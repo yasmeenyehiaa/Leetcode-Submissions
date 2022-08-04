d4ee36c3bea79bb41e1e0645890435bab9c3d92a
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-08-04 17:20:43 +0200

    Time: 92 ms (57.14%), Space: 24.1 MB (16.14%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int dx[4] = {0,0,1,-1}, dy[4] = {1,-1,0,0}, ans = 0, n,m;
+    vector<vector<int>> g;
+    bool valid(int x, int y)
+    {
+        return x>=0 &&  x<n && y>=0 && y<m; 
+    }
+    int dfs(int x, int y)
+    {
+        if(g[x][y] == 0) return 0;
+        g[x][y] = 0;
+        int ret = 1;
+        
+        for(int i=0; i<4; i++)
+        {
+            int nx = x+dx[i], ny = y+dy[i];
+            if(valid(nx, ny))
+            {
+                ret += dfs(nx, ny);
+            }
+            else ret += 1e7;
+        }
+        
+        return ret;
+        
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        
+        n = grid.size(), m = grid[0].size();
+        g = grid;
+        
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(g[i][j] == 1)
+                {
+                    int c = dfs(i,j);
+                    if(c < 1e7) ans += c;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
