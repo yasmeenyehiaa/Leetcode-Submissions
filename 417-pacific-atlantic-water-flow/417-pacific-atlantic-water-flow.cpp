05dce6c8ae6fca448aebeb3d7cbdd2ccd12a1c7d
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-08-05 17:59:23 +0200

    Time: 431 ms (6.75%), Space: 17.1 MB (98.30%) - LeetHub

---
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int dx[4] = {0,0,1,-1}, dy[4] = {1,-1,0,0},n,m, P_ok, A_ok;
+    bool vis[205][205];
+    bool valid(int x, int y)
+    {
+        return x>=0 && y>=0 && x<n && y<m;
+    }
+    void dfs(int x, int y, vector<vector<int>>& heights)
+    {
+        vis[x][y] = 1;
+        for(int i=0; i<4; i++)
+        {
+            int nx = x+dx[i], ny = y+dy[i];
+            if(valid(nx,ny) && !vis[nx][ny] && heights[nx][ny] <= heights[x][y]) dfs(nx,ny,heights);
+            else if(nx < 0 || ny < 0) P_ok = 1;
+            else if(nx >= n || ny >= m) A_ok = 1;
+        }
+    }
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        
+        vector<vector<int>> ans;
+        n = heights.size(), m = heights[0].size();
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                memset(vis, 0, sizeof vis);
+                P_ok = A_ok = 0;
+                dfs(i,j,heights);
+                if(P_ok && A_ok) 
+                {
+                    ans.push_back({i,j});
+                }   
+            }
+        }
+        return ans;
+    }
+};
