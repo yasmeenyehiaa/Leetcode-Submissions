d681dcd04d0c0acb4e125e0c0c2b6735bfdfd8ca
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-08-05 17:25:43 +0200

    Time: 3 ms (62.90%), Space: 7.7 MB (5.01%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[205][1005];
+    vector<int> v;
+    int solve(int i, int rem)
+    {
+        if(i == v.size() || rem == 0) return rem == 0;
+        if(dp[i][rem] != -1) return dp[i][rem];
+        
+        int op1 = 0, op2 = 0;
+        
+        if(v[i] <= rem) op1 = solve(0, rem-v[i]);
+        op2 = max(op2, solve(i+1, rem));
+        
+        return dp[i][rem] = op1 + op2;
+    }
+    int combinationSum4(vector<int>& nums, int t) {
+        memset(dp, -1, sizeof dp);
+        v = nums;
+        //sort(nums.begin(), nums.end());
+        return solve(0, t);
+    }
+};
