7f2a99292ad0c6696c7459295348e03ea454df4f
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-08-11 00:12:38 +0200

    Time: 362 ms (84.24%), Space: 106.8 MB (24.66%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
         
-    vector<int>ans;
-    int in[100005] = {0}, out[100005] = {0};
-    for(auto i:edges)
-    {
-        in[i[1]]++;
-        out[i[0]]++;
-    }
-    for(int i=0;i<n;i++)
-    {
-        if(in[i]==0 && out[i]!=0)
+        vector<int>ans;
+        int in[100005] = {0}, out[100005] = {0};
+        for(auto i:edges)
         {
-            ans.push_back(i);
+            in[i[1]]++;
+            out[i[0]]++;
         }
-    }
-    return ans;
+        for(int i=0;i<n;i++)
+        {
+            if(in[i]==0 && out[i]!=0)
+            {
+                ans.push_back(i);
+            }
+        }
+        return ans;
     }
 };
