48966c79c65007de925115cf0cccb3d10247b701
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-08-12 21:58:08 +0200

    Time: 11 ms (79.27%), Space: 7.1 MB (12.56%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         
-    int mx = 0, n = s.size(), l = 0, ans = 0;
-    unordered_map<char,int> cnt;
-    for(int r=0; r<n; r++)
-    {
-      cnt[s[r]]++;
-      mx = max(mx, cnt[s[r]]);
-      while(l<n && r-l+1-mx > k)
-      { 
-        cnt[s[l]]--;
-        l++;
-      }
-      ans = max(ans, r-l+1);
-    }
-    return ans;
-        
+        int mx = 0, n = s.size(), l = 0, ans = 0;
+        unordered_map<char,int> cnt;
+        for(int r=0; r<n; r++)
+        {
+          cnt[s[r]]++;
+          mx = max(mx, cnt[s[r]]);
+          while(l<n && r-l+1-mx > k)
+          { 
+            cnt[s[l]]--;
+            l++;
+          }
+          ans = max(ans, r-l+1);
+        }
+        return ans;        
     }
 };
