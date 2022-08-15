b3d69507dedb3bbf78aff7cb2150a186668f2d22
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-08-15 05:14:06 +0200

    Time: 44 ms (33.16%), Space: 7.2 MB (99.48%) - LeetHub

---
+++ 457-circular-array-loop/457-circular-array-loop.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int getNxt(int i, int d, vector<int>& nums)
+    {
+        int dir = (nums[i]>=0), n = nums.size();
+        if(d!=dir) return -1;
+        int nxt = (i+nums[i])%(n);
+        if(nxt < 0) nxt += n;
+        
+        if(nxt == i) return -1;
+        return nxt;
+    }
+    bool circularArrayLoop(vector<int>& nums) {
+        
+        int n = nums.size();
+        for(int i=0; i<n; i++)
+        {
+            int dir = (nums[i] >= 0), slow = i, fast = i;
+            do
+            {
+                slow = getNxt(slow, dir, nums);
+                fast = getNxt(fast, dir, nums);
+                if(fast != -1) fast = getNxt(fast, dir, nums);
+            }
+            while(slow!=fast && slow!=-1 && fast!=-1);
+            if(fast==slow && slow!=-1) return true;
+        }
+        return false;
+    }
+};
