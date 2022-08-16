ffa21e08252e13fc871baae7da91e6c7d724dfdf
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-08-16 22:25:12 +0200

    Time: 6 ms (21.06%), Space: 5.9 MB (26.88%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        while(n>=2 && n%2 == 0)
+        {
+            n /= 2;
+        }        
+        return n == 1;
+        
+    }
+};
