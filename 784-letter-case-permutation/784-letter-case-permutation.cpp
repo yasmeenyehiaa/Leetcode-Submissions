14de39948d616087999e43fad70412ebe92c012e
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-08-16 21:59:30 +0200

    Time: 15 ms (34.37%), Space: 12.3 MB (13.25%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> ans;
+    void solve(int i, string s, string word)
+    {
+        if(word.size() == s.size()) 
+        {
+            ans.push_back(word);
+            return;
+        }
+        
+        if(s[i] <= '9' && s[i] >= '0')
+        {
+            word += s[i];
+            solve(i+1, s, word);
+        }
+        else
+        {
+            word.push_back(toupper(s[i]));
+            solve(i+1, s, word);
+            word.pop_back();
+            word.push_back(tolower(s[i]));
+            solve(i+1, s, word);
+        }
+    }
+    vector<string> letterCasePermutation(string s) {
+        solve(0, s, "");
+        return ans;
+    }
+};
