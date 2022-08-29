e5f4ff8e8ced4b7458b73f9fd26255ac94bb692e
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-08-29 18:18:44 +0200

    Time: 9 ms (85.88%), Space: 10 MB (46.67%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.cpp
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -5,20 +5,20 @@ public:
         int num = 0;
         for(int i=0; i<nums.size(); i++)
         {
-          num = num ^ nums[i];
+           num = num ^ nums[i];
         }
         int n1 = 0, n2 = 0, rightMostBit = 1;
         while((rightMostBit & num) == 0)
         {
-          rightMostBit = rightMostBit << 1;
+           rightMostBit = rightMostBit << 1;
         }
         for(auto n: nums)
         {
-          if(n & rightMostBit)
-          {
-            n1 ^= n;
-          }
-          else n2 ^= n;
+           if(n & rightMostBit)
+           {
+             n1 ^= n;
+           }
+           else n2 ^= n;
         }
         return vector<int>{n1,n2};
     }
