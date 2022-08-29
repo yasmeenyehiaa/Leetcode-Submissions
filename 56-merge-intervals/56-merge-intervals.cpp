e8619598f29333b7d2c5ba28e99339c5670e8989
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-08-29 02:54:12 +0200

    Time: 78 ms (27.46%), Space: 20.1 MB (14.82%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>>  merge(vector<vector<int>>& intervals) {
+        
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end());
+        int start = intervals[0][0], end = intervals[0][1];
+        for(auto a: intervals)
+        {
+            if(a[0] <= end)
+            {
+                end = max(end, a[1]);
+            }
+            else 
+            {
+                ans.push_back({start, end});
+                start = a[0], end = a[1];
+            }
+        }
+        ans.push_back({start, end});
+        return ans;        
+    }
+};
