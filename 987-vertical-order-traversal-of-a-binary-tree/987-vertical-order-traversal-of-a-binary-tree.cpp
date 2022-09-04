9c1921efd99746b80eb9878c2aa1e85826e1c284
Yasmeen Yehia <102305226+yasmeenyehiaa@users.noreply.github.com>
2022-09-05 01:31:18 +0200

    Time: 9 ms (49.02%), Space: 12.1 MB (84.54%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,24 +12,26 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-	map<int, vector<int> > m;  
+	map<int, vector<int> > mp;  
 	queue<pair<int, TreeNode*> > q; 
 	q.push(make_pair(0, root)); 
-	while(!q.empty()) {
+	while(!q.empty()) 
+    {
 		multiset<pair<int, int> > tmp;  
 		int len=q.size();
-		for(int i=0;i<len;++i) {
-			auto p=q.front();q.pop();
+		for(int i=0; i<len; i++) 
+        {
+			auto p=q.front();
+            q.pop();
 			tmp.insert(make_pair(p.first, p.second->val));
 			if (p.second->left) q.push(make_pair(p.first-1, p.second->left));
 			if (p.second->right) q.push(make_pair(p.first+1, p.second->right));
 		}
-
-		for(auto p : tmp) m[p.first].push_back(p.second);
+		for(auto p : tmp) mp[p.first].push_back(p.second);
 	}
 
 	vector<vector<int> > res;
-	for(auto kv : m) res.push_back(kv.second);
+	for(auto kv : mp) res.push_back(kv.second);
 	return res;
     }
 };
